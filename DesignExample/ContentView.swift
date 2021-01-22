7dfbbd67ede7d2bc3ef5bbfe58bead5760b350d5
denvolkov <denvolkov@inbox.ru>
2021-01-22 15:48:11 +0300

    added card

--- DesignExample/ContentView.swift
+++ DesignExample/ContentView.swift
@@ -2,15 +2,43 @@
 //  ContentView.swift
 //  DesignExample
 //
-//  Created by test on 22.01.2021.
+//  Created by Denis Volkov on 22.01.2021.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+        HStack {
+            VStack {
+                Text("Дизайн в SwiftUI")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                Text("Сертификат")
+                    .foregroundColor(Color("Primary"))
+            }
+            
+            Spacer()
+            
+            Image("Logo SwiftUI")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50)
+        }
+        .padding(.horizontal, 20)
+        .padding(.top, 20)
+            
+            Image("2")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 110, alignment: .top)
+    }
+        .frame(width: 340, height: 220)
+        .background(Color.black)
+        .cornerRadius(20)
+        .shadow(radius: 20)
     }
 }
 
