5e7f9938eb2b304746d64b0d5ba47fd33bc499ca
denvolkov <denvolkov@inbox.ru>
2021-01-22 14:37:52 +0300

    Initial Commit

---
+++ DesignExample/DesignExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignExampleApp.swift
+//  DesignExample
+//
+//  Created by test on 22.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct DesignExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
