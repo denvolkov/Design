7f110ae65feede0d443f61fcadf941380844e155
denvolkov <denvolkov@inbox.ru>
2021-01-25 16:26:32 +0300

    Added Home Page

---
+++ DesignExample/HomeView.swift
@@ -0,0 +1,29 @@
+//
+//  HomeView.swift
+//  DesignExample
+//
+//  Created by test on 25.01.2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @Binding var showProfile: Bool
+    var body: some View {
+        VStack {
+            HStack {
+                AvatarView(showProfile: $showProfile)
+                Spacer()
+            }
+            .padding(.horizontal)
+            .padding(.top, 30)
+            Spacer()
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView(showProfile: Binding<Bool>.constant(false))
+    }
+}
--- DesignExample/MenuView.swift
+++ DesignExample/MenuView.swift
@@ -9,12 +9,66 @@ import SwiftUI
 
 struct MenuView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            VStack(spacing: 16) {
+                Text("Обучение завершено на 23 %")
+                    .font(.caption)
+                Color.white
+                    .frame(width: 38, height: 6)
+                    .cornerRadius(3)
+                    .frame(width: 130, height: 6, alignment: .leading)
+                    .background(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.08))
+                    .cornerRadius(3)
+                    .padding()
+                    .frame(width: 160, height: 24)
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(12)
+                
+                MenuRow(title: "Личный кабинет", icon: "gear")
+                MenuRow(title: "Счет", icon: "creditcard")
+                MenuRow(title: "Выход", icon: "person.crop.circle")
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 300)
+            .background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.8505654642, green: 0.8505654642, blue: 0.8505654642, alpha: 1)), Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))]), startPoint: .top, endPoint: .bottom))
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+            .padding(.horizontal, 30)
+            .overlay(
+                Image("avatar")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 60, height: 60)
+                    .clipShape(Circle())
+                    .offset(y: -150)
+            )
+        }
+        .padding(.bottom, 30)
     }
 }
 
 struct MenuView_Previews: PreviewProvider {
     static var previews: some View {
         MenuView()
+    }
+}
+
+struct MenuRow: View {
+    var title: String
+    var icon: String
+    
+    var body: some View {
+        HStack(spacing: 16) {
+            Image(systemName: icon)
+                .font(.system(size: 20, weight: .light, design: .default))
+                .imageScale(.large)
+                .frame(width: 32, height: 32)
+                .foregroundColor(Color(#colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1)))
+            Text(title)
+                .font(.system(size: 20, weight: .bold, design: .rounded))
+                .frame(width: 120, alignment: .leading)
+        }
+        
     }
 }
