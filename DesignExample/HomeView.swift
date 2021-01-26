32fc28452dd52e064fbb2a66dff90cdbe042c439
denvolkov <denvolkov@inbox.ru>
2021-01-26 12:53:19 +0300

    Added scroll cards in Home page

--- DesignExample/HomeView.swift
+++ DesignExample/HomeView.swift
@@ -16,7 +16,23 @@ struct HomeView: View {
                 Spacer()
             }
             .padding(.horizontal)
+            .padding(.leading, 14)
             .padding(.top, 30)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 20) {
+                    ForEach(sectionData) { item in
+                        GeometryReader { geometry in
+                        SectionView(section: item)
+                            .rotation3DEffect(Angle(degrees: Double(geometry.frame(in: .global) .minX - 30) / -20), axis: (x: 0, y: 10, z: 0))
+                    }
+                        .frame(width: 275, height: 275)
+                    }
+                }
+                .padding(30)
+                .padding(.bottom, 30)
+            }
+            
             Spacer()
         }
     }
@@ -27,3 +43,50 @@ struct HomeView_Previews: PreviewProvider {
         HomeView(showProfile: .constant(false))
     }
 }
+
+struct SectionView: View {
+    var section: Section
+    var body: some View {
+        VStack {
+            HStack(alignment: .top) {
+                Text(section.title)
+                    .font(.system(size: 20, weight: .bold))
+                    .frame(width: 160, alignment: .leading)
+                    .foregroundColor(.white)
+                
+                Spacer()
+                
+                Image(section.logo)
+            }
+            Text(section.text.uppercased())
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            section.image
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 210)
+        }
+        .padding(.top, 20)
+        .padding(.horizontal, 20)
+        .frame(width: 275, height: 275)
+        .background(section.color)
+        .cornerRadius(30)
+        .shadow(color: section.color.opacity(0.3), radius: 20, x: 0, y: 20)
+        
+    }
+}
+
+struct Section: Identifiable {
+    var id = UUID()
+    var title: String
+    var text: String
+    var logo: String
+    var image: Image
+    var color: Color
+}
+
+let sectionData = [
+    Section(title: "Дизайн в SwiftUI", text: "50 уроков", logo: "Logo SwiftUI", image: Image("1"), color: Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1))),
+    Section(title: "Основы Swift", text: "30 уроко", logo: "Logo SwiftUI", image: Image("2"), color: Color(#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1))),
+    Section(title: "Решение задач на языке Swift", text: "35 уроко", logo: "Logo SwiftUI", image: Image("4"), color: Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)))
+]
